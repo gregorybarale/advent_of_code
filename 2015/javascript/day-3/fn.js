8ecc2fd1a0ec1b824c32c1a61a5704233891c475
Grégory Barale <barale.g@sfeir.com>
2019-08-29 17:34:20 +0200

    Add 2015 javascript day 3

---
+++ 2015/javascript/day-3/fn.js
@@ -0,0 +1,88 @@
+const fn1 = input => {
+  const intructions = input.split("");
+  const coords = { latitude: 0, longitude: 0 };
+  const visitedHousesCoords = {
+    "0x0": 1
+  };
+  intructions.forEach(element => {
+    switch (element) {
+      case ">":
+        coords.longitude = coords.longitude + 1;
+        break;
+      case "<":
+        coords.longitude = coords.longitude - 1;
+        break;
+      case "^":
+        coords.latitude = coords.latitude + 1;
+        break;
+      case "v":
+        coords.latitude = coords.latitude - 1;
+        break;
+    }
+    const visitedHouseCoord = `${coords.latitude}x${coords.longitude}`;
+    if (visitedHousesCoords[visitedHouseCoord]) {
+      visitedHousesCoords[visitedHouseCoord] =
+        visitedHousesCoords[visitedHouseCoord] + 1;
+    } else {
+      visitedHousesCoords[visitedHouseCoord] = 1;
+    }
+  });
+  return Object.keys(visitedHousesCoords).length;
+};
+
+const fn2 = input => {
+  const intructions = input.split("");
+  const santaCoords = { latitude: 0, longitude: 0 };
+  const robotCoords = { latitude: 0, longitude: 0 };
+  const visitedHousesCoords = {
+    "0x0": 1
+  };
+  intructions.forEach((element, index) => {
+    let visitedHouseCoord;
+    if (index % 2 === 0) {
+      switch (element) {
+        case ">":
+          santaCoords.longitude = santaCoords.longitude + 1;
+          break;
+        case "<":
+          santaCoords.longitude = santaCoords.longitude - 1;
+          break;
+        case "^":
+          santaCoords.latitude = santaCoords.latitude + 1;
+          break;
+        case "v":
+          santaCoords.latitude = santaCoords.latitude - 1;
+          break;
+      }
+      visitedHouseCoord = `${santaCoords.latitude}x${santaCoords.longitude}`;
+    } else {
+      switch (element) {
+        case ">":
+          robotCoords.longitude = robotCoords.longitude + 1;
+          break;
+        case "<":
+          robotCoords.longitude = robotCoords.longitude - 1;
+          break;
+        case "^":
+          robotCoords.latitude = robotCoords.latitude + 1;
+          break;
+        case "v":
+          robotCoords.latitude = robotCoords.latitude - 1;
+          break;
+      }
+      visitedHouseCoord = `${robotCoords.latitude}x${robotCoords.longitude}`;
+    }
+    if (visitedHousesCoords[visitedHouseCoord]) {
+      visitedHousesCoords[visitedHouseCoord] =
+        visitedHousesCoords[visitedHouseCoord] + 1;
+    } else {
+      visitedHousesCoords[visitedHouseCoord] = 1;
+    }
+  });
+  return Object.keys(visitedHousesCoords).length;
+};
+
+module.exports = {
+  fn1,
+  fn2
+};
---
+++ 2015/javascript/day-3/test.js
@@ -0,0 +1,34 @@
+const assert = require("assert");
+const fn = require("./fn");
+
+const testCases1 = [
+  { input: ">", result: 2 },
+  { input: "^>v<", result: 4 },
+  { input: "^v^v^v^v^v", result: 2 }
+];
+
+const testCases2 = [
+  { input: "^v", result: 3 },
+  { input: "^>v<", result: 3 },
+  { input: "^v^v^v^v^v", result: 11 }
+];
+
+testCases1.forEach(testCase => {
+  assert.equal(
+    fn.fn1(testCase.input),
+    testCase.result,
+    `Expect ${testCase.result} for ${
+      testCase.input
+    } input in part 1 instead of ${fn.fn1(testCase.input)}`
+  );
+});
+
+testCases2.forEach(testCase => {
+  assert.equal(
+    fn.fn2(testCase.input),
+    testCase.result,
+    `Expect ${testCase.result} for ${
+      testCase.input
+    } input in part 2 instead of ${fn.fn2(testCase.input)}`
+  );
+});
