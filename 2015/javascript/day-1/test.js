c653ae5d1835626978b9dc0a9634f52d53e2b9a7
Grégory Barale <barale.g@sfeir.com>
2019-08-29 17:03:05 +0200

    Update test for 2015 javascript day 1

--- 2015/javascript/day-1/test.js
+++ 2015/javascript/day-1/test.js
@@ -1,27 +1,36 @@
-const assert = require('assert');
-const fn = require('./fn');
+const assert = require("assert");
+const fn = require("./fn");
 
 const testCases1 = [
-    {input: "(())", result: 0},
-    {input: "()()", result: 0},
-    {input: "(((", result: 3},
-    {input: "(()(()(", result: 3},
-    {input: "))(((((", result: 3},
-    {input: "())", result: -1},
-    {input: "))(", result: -1},
-    {input: ")))", result: -3},
-    {input: ")())())", result: -3}
+  { input: "(())", result: 0 },
+  { input: "()()", result: 0 },
+  { input: "(((", result: 3 },
+  { input: "(()(()(", result: 3 },
+  { input: "))(((((", result: 3 },
+  { input: "())", result: -1 },
+  { input: "))(", result: -1 },
+  { input: ")))", result: -3 },
+  { input: ")())())", result: -3 }
 ];
+
+const testCases2 = [{ input: ")", result: 1 }, { input: "()())", result: 5 }];
 
 testCases1.forEach(testCase => {
-    assert.equal(fn.fn1(testCase.input), testCase.result, `Expect ${testCase.result} for ${testCase.input} input in part 1`);
+  assert.equal(
+    fn.fn1(testCase.input),
+    testCase.result,
+    `Expect ${testCase.result} for ${
+      testCase.input
+    } input in part 1 instead of ${fn.fn1(testCase.input)}`
+  );
 });
 
-const testCases2 = [
-    {input: ")", result: 1},
-    {input: "()())", result: 5}
-];
-
 testCases2.forEach(testCase => {
-    assert.equal(fn.fn2(testCase.input), testCase.result, `Expect ${testCase.result} for ${testCase.input} input in part 2`);
+  assert.equal(
+    fn.fn2(testCase.input),
+    testCase.result,
+    `Expect ${testCase.result} for ${
+      testCase.input
+    } input in part 2 instead of ${fn.fn2(testCase.input)}`
+  );
 });
