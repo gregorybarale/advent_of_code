7ecec31c1ac2ba586cd7e17f0ff20e0873a96068
Grégory Barale <barale.g@sfeir.com>
2019-08-30 09:59:37 +0200

    Add 2015 javascript day 4

---
+++ 2015/javascript/day-4/fn.js
@@ -0,0 +1,28 @@
+const crypto = require("crypto");
+
+function getHash(s) {
+  const hash = crypto.createHash("md5");
+  hash.update(s);
+  return hash.digest("hex");
+}
+
+const fn1 = input => {
+  let n = 1;
+  while (getHash(`${input}${n}`).substring(0, 5) != "00000") {
+    n++;
+  }
+  return n;
+};
+
+const fn2 = input => {
+  let n = 254575;
+  while (getHash(`${input}${n}`).substring(0, 6) != "000000") {
+    n++;
+  }
+  return n;
+};
+
+module.exports = {
+  fn1,
+  fn2
+};
---
+++ 2015/javascript/day-4/test.js
@@ -0,0 +1,17 @@
+const assert = require("assert");
+const fn = require("./fn");
+
+const testCases1 = [
+  { input: "abcdef", result: 609043 },
+  { input: "pqrstuv", result: 1048970 }
+];
+
+testCases1.forEach(testCase => {
+  assert.equal(
+    fn.fn1(testCase.input),
+    testCase.result,
+    `Expect ${testCase.result} for ${
+      testCase.input
+    } input in part 1 instead of ${fn.fn1(testCase.input)}`
+  );
+});
