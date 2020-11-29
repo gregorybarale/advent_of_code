b0c713038172c9a9b80bf912d7477383582a0c89
Grégory Barale <barale.g@sfeir.com>
2020-11-29 23:00:59 +0100

    Add 2015 day 8 javascript

---
+++ 2015/javascript/day-8/fn.js
@@ -0,0 +1,24 @@
+const { txtToArray } = require('../input-utils/txt-to-array');
+
+const fn1 = input => {
+  const max = txtToArray(input)[0].replace(/\\"/g, '##').replace(/\\\\/g, '##').replace(/(\\x([0-9]|[a-f]|[A-F]){2})/g, '####').length;
+  const min = (txtToArray(input)[0].toString()).length
+  //const minus = array.reduce((acc, s) => acc + s.length, 0);
+  console.log(txtToArray(input)[0].toString())
+  console.log(max, min)
+  return undefined;
+};
+
+const fn2 = input => {
+  return undefined;
+};
+
+module.exports = {
+  fn1,
+  fn2
+};
+
+console.log(txtToArray('./input.txt')[0].length);
+console.log(txtToArray('./input.txt')[0].replace(/\\"/g, '##').replace(/\\\\/g, '##').replace(/(\\x([0-9]|[a-f]|[A-F]){2})/g, '####').length);
+
+fn1('./input.txt');
