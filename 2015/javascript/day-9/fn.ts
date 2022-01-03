c4c38bbe1c6884a05a6bb7996544bb9d49cdf3c5
Grégory Barale <barale.g@sfeir.com>
2022-01-03 19:22:41 +0100

    Add 2015 day 9 javascript

---
+++ 2015/javascript/day-9/fn.ts
@@ -0,0 +1,10 @@
+import { IAoCInput } from "../../../utils/javascript/deno-utils/input.ts";
+
+export const fn1 = (input: IAoCInput) => {
+  console.info("[2015-9] fn1", input);
+  return undefined;
+};
+export const fn2 = (input: IAoCInput) => {
+  console.info("[2015-9] fn2", input);
+  return undefined;
+};
